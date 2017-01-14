273731c3f87921851f536e981c5eda444342fa51
Itay Lotan <itay235711@gmail.com>
2017-01-14 12:48:31 +0200

    add installation to ffmpeg

---
+++ prerequisites/ffmpeg/set_enviroment_variables.bat
@@ -0,0 +1,7 @@
+:: change the <> vales to the path on your machine and run from admin command prompt
+
+setx FFMPEG_PATH <path to ffmpeg.exe in ffmpeg bin dir> /M
+setx FFPROBE_PATH <path to ffprobe.exe in ffmpeg bin dir> /M
+
+
+
