fb2e2b4ba7534b24c999a2685c264b4083d38aa0
Itay Lotan <itay235711@gmail.com>
2017-04-10 18:17:57 +0300

    google api working sample

--- shazam_gmaillabel_extractor.js
+++ shazam_gmaillabel_extractor.js
@@ -1,3 +1,36 @@
 /**
  * Created by itay on 4/10/2017.
  */
+
+const google = require('googleapis');
+const aoth_authenticator = require('./google_api/aoth_authenticator.js');
+
+aoth_authenticator.authenticate().then(listLabels);
+
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listLabels(auth) {
+    var gmail = google.gmail('v1');
+    gmail.users.labels.list({
+        auth: auth,
+        userId: 'me',
+    }, function(err, response) {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+        }
+        var labels = response.labels;
+        if (labels.length == 0) {
+            console.log('No labels found.');
+        } else {
+            console.log('Labels:');
+            for (var i = 0; i < labels.length; i++) {
+                var label = labels[i];
+                console.log('- %s', label.name);
+            }
+        }
+    });
+}
