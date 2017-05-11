6dcd153a1564ca06dafda8b928d101933511e88b
Itay Lotan <itay235711@gmail.com>
2017-05-12 01:19:28 +0300

    added run modes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 const _mailGoogleUser = 'itay235711';
 const _driveGoogleUser = 'SongsSharer2';
 
-const ysd = require('./youtube-songs-downloader.js')();
+const newYsd = require('./youtube-songs-downloader.js');
 const extractor = require('./shazam_gmaillabel_extractor.js')(_mailGoogleUser);
 const driveUploader = require('./songs_drive_uploader.js')(_driveGoogleUser);
 const aoth_authenticator = require('./google_api/aoth_authenticator.js');
@@ -15,24 +15,103 @@ const projectUtils = require('./utils.js');
 const cp = require('child_process');
 const Promise = require("bluebird");
 const denodeify = require('promise-denodeify');
+const optimist = require('optimist');
+const lineReader =  require('line-reader');
 
 initCallbacksBehavior();
 
 
-downloadShazamNewSongs();
+const MAIN_OUTPUT_DIR = 'C:\\Users\\itay\\home\\7_temp\\boazTestsDir\\';
+const runConfig = extractConfigFromAppArgs();
+
+switch (runConfig.mode) {
+    case 'input_file':
+        downloadSongTitlesFromFile(runConfig.file_path);
+        break;
+    case 'single_title':
+        downloadSongTitles([runConfig.title], MAIN_OUTPUT_DIR);
+        break;
+    case 'yedidyas_shazam':
+        downloadShazamNewSongs();
+        break;
+    default:
+        throw new Exception("unknown run mode '" + runConfig + "'");
+}
+
+function extractConfigFromAppArgs() {
+    const runConfig = {};
+
+    runConfig.mode = optimist
+        .usage('Usage: node $0 --mode=[run mode]')
+        .demand(['mode'])
+        .argv.mode;
+
+    switch (runConfig.mode) {
+        case 'input_file':
+            runConfig.file_path = optimist
+                .usage('Usage: node $0 --mode=input_file --file_path=[path to input file]')
+                .demand(['file_path'])
+                .argv.file_path;
+            break;
+        case 'single_title':
+            runConfig.title = optimist
+                .usage('Usage: node $0 --mode=single_title --title=[song title]')
+                .demand(['file_path'])
+                .argv.title;
+            break;
+        case 'yedidyas_shazam':
+            break;
+        default:
+            throw new Exception("unknown run mode '" + runConfig + "'");
+    }
+
+    return runConfig;
+}
+
+function downloadSongTitles(songTitles, outputDir) {
+
+    const ysd = initNewYsdWithDefaults();
+    ysd.setOutputDir(outputDir);
+
+    projectUtils.createDirIfNotExists(outputDir)
+        .then(() => ysd.downloadSongsList(songTitles))
+        .then(() => {
+            cp.exec('start "" "' + outputDir + '"', () => {
+                    console.log('done.');
+                    process.exit(0);
+                }
+            );
+    }).catch(err => {
+        console.log(err);
+        process.exit(1);
+    });
+}
+
+function downloadSongTitlesFromFile(inputFilePath) {
+    const songTitles = [];
+
+    lineReader.eachLine(inputFilePath, line => {
+        if (!!line) {
+            songTitles.push(line);
+        }
+    }).then(err => {
+        if (err)
+            throw err;
+
+        const fileName = path.parse(inputFilePath).name;
+        const outputDirPath = path.join(MAIN_OUTPUT_DIR, '\\', fileName);
+        downloadSongTitles(songTitles, outputDirPath);
+    });
+}
 
 function downloadShazamNewSongs() {
 
-
     extractor.extractShazamLabelNewSongTitles().then(songTitles =>{
-        const mainOutputDir = 'C:\\Users\\itay\\home\\7_temp\\boazTestsDir\\';
 
-        const todaysOutputDir = createTodaysOutputDir(mainOutputDir);
+        const todaysOutputDir = createTodaysOutputDir(MAIN_OUTPUT_DIR);
 
-        ysd.setOutputDir(todaysOutputDir)
-            .setMaxYtSearchResultsNumber(3)
-            .setMaxSongDurationMinutes(30)
-            .setMaxSongFileSizeOptions({ prefferedSizeLimitMB: 50, maxSizeLimitMB: 80});
+        const ysd = initNewYsdWithDefaults();
+        ysd.setOutputDir(todaysOutputDir);
 
         ysd.assignOnSongDownloaded(extractor.markMessageAsReadBySongTitle);
         ysd.assignOnSongDownloadError(extractor.markMessageAsProblematicBySongTitle);
@@ -52,6 +131,15 @@ function downloadShazamNewSongs() {
             });
         });
     });
+}
+
+function initNewYsdWithDefaults() {
+    const ysd = newYsd();
+    ysd.setMaxYtSearchResultsNumber(3)
+       .setMaxSongDurationMinutes(30)
+       .setMaxSongFileSizeOptions({ prefferedSizeLimitMB: 50, maxSizeLimitMB: 80});
+
+    return ysd;
 }
 
 function reportErrorViaMail(err) {
@@ -81,4 +169,5 @@ function createTodaysOutputDir(outputDirPath) {
 
 function initCallbacksBehavior() {
     cp.exec = denodeify(cp.exec, Promise, false);
+    lineReader.eachLine = denodeify(lineReader.eachLine, Promise, false);
 }
